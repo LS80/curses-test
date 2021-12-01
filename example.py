7c9a6512b90c543569c2c074664aa5248a2ad6d3
Lee Smith <lee.smith@fundingcircle.com>
2021-12-01 22:54:06 +0000

    debug

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 import curses
 import time
 import sys
+import os
 
 class A():
     def draw(self, stdscr):
@@ -11,6 +12,10 @@ class A():
             time.sleep(2)
 
 a = A()
+
+print(sys.stdout.isatty())
+print(os.environ['TERM'])
+
 if sys.stdout.isatty():
     curses.wrapper(a.draw)
 else:
